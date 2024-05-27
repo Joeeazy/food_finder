7d0dde1e3b0b413912d35438af83c7e80d2f103a
Joeeazy <joeeazy34@gmail.com>
2024-05-27 10:46:16 +0300

    MongDB setup complete

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,54 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+require("dotenv").config();
+
+// middlware implementation
+app.use(cors());
+app.use(express.json());
+
+// mongodb config code
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@foodie-cluster.3iohndh.mongodb.net/?retryWrites=true&w=majority&appName=foodie-cluster`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // connect to mongodb
+    await client.connect();
+    // create databases define database and collections
+    const menuCollections = client.db("demo-foodi-client").collection("menus");
+    const cartCollections = client
+      .db("demo-foodi-client")
+      .collection("cartItems");
+
+    // all menu items operations
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollections.find().toArray();
+      res.send(result);
+    });
+
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    //await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
